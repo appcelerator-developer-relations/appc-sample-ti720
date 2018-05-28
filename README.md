9d36abdcdc29abd8896b226049d1c3095469832c
Rene Pot <rene@topener.nl>
2018-05-28 17:20:10 +0200

    add toggle button

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Sample app for TiSDK 7.2.0.GA
 
-This sample is for showing 
+This sample is for showing how to let users opt out from Analytics, following the GPDR laws, and how to let users give consent using ti.admob
+
+## Analytics opt-out
+
+Using `Ti.Analytics`  you can let users opt out of your app. This is demonstrated in the index controller. For this you can use the `Ti.Analytics.getOptedOut()` method.
+
+Opting the user out is done with the related setter. `Ti.Analytics.setOptedOut(true)`. Opting in again of course replaces `true` with `false`.
+
+How to implement this is demonstrated in this sample app.
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,11 +7,22 @@ $.getView().open();
 function checkStatus(){
 	var status = Ti.Analytics.getOptedOut();
 	Ti.API.info('status: ' + status);
-	if (status === 1){
+	if (status === true){
 		$.resetClass($.status, ['status_disabled']);
+		$.resetClass($.toggleButton, ['button_enable']);
 	} else {
 		$.resetClass($.status, ['status_enabled']);
+		$.resetClass($.toggleButton, ['button_disable']);
 	}
 }
 
+function handleToggleOptoutButton(){
+	var status = Ti.Analytics.getOptedOut();
+
+	// inverse boolean value
+	Ti.Analytics.setOptedOut(!status);
+	checkStatus();
+}
+
+// start app with checking status
 checkStatus();
