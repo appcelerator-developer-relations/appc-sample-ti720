b0ddd2aca731222e32dd72db2c74b977873818ef
Rene Pot <rene@topener.nl>
2018-05-31 13:12:55 +0200

    linting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,26 +2,26 @@ $.getView().open();
 
 // check the status
 function checkStatus() {
-	let optOut = Ti.Analytics.getOptedOut();
+	const optedOut = Ti.Analytics.getOptedOut();
 
 	// send status to logs
-	Ti.API.info('status: ' + optOut);
-	if (optOut === true) {
-		// optOut is true here
+	Ti.API.info('status: ' + optedOut);
+	if (optedOut === true) {
+		// optedOut is true here
 		$.resetClass($.status, ['status_disabled']);
 		$.resetClass($.toggleButton, ['button_enable']);
 	} else {
-		// optOut is false or not defined, both reflect an optIn
+		// optedOut is false or not defined, both reflect an optIn
 		$.resetClass($.status, ['status_enabled']);
 		$.resetClass($.toggleButton, ['button_disable']);
 	}
 
-	let result = Ti.Analytics.featureEvent('isActive', {active: optOut});
+	const result = Ti.Analytics.featureEvent('isActive', { active: optedOut });
 	Ti.API.info('analytics result: ' + result);
 }
 
 function handleToggleOptoutButton() {
-	let status = Ti.Analytics.getOptedOut();
+	const status = Ti.Analytics.getOptedOut();
 
 	// inverse boolean value
 	Ti.Analytics.setOptedOut(!status);
